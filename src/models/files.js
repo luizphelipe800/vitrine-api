a3627cbadfb93aa346aae91872238488f6ca103e
Luiz Felipe Santos <luiz.phelipe.800@gmail.com>
2021-01-14 18:35:30 -0300

    fix[files/model] adicionado um campo para a url completa do arquivo

--- src/models/files.js
+++ src/models/files.js
@@ -1,7 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
   const files = sequelize.define('files', {
     path: DataTypes.STRING,
-    shopId: DataTypes.INTEGER
+    shopId: DataTypes.INTEGER,
+    url: {
+      type: DataTypes.VIRTUAL,
+      get: function(){
+        return `http://localhost:3000/${this.path}`
+      }
+    }
   });
 
   files.associate = models => {
